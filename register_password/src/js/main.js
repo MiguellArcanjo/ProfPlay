13cd4e30983ddddceaf36dd8bd81a3251d5f19d6
Gusta <gutolights0@gmail.com>
2023-04-12 16:07:29 -0300

    Não lembro o que eu fiz

--- register_password/src/js/main.js
+++ register_password/src/js/main.js
@@ -3,20 +3,42 @@ const password2 = document.querySelector('.senha2')
 const olhin1 = document.querySelector('.olhin');
 const olhin2 = document.querySelector('.olhin1')
 
-function showHide(){
+function showHide1(){
 
-    if(password1.type === 'password' && password2.type === 'password'){
+    if(password1.type === 'password'){
         password1.setAttribute('type', 'text');
-        password2.setAttribute('type', 'text');
         olhin1.classList.add('hide1');
-        olhin2.classList.add('hide2');
 
     }
     else{
         password1.setAttribute('type', 'password');
-        password2.setAttribute('type', 'password');
         olhin1.classList.remove('hide1');
+    }
+
+}
+
+function showHide2(){
+
+    if(password2.type === 'password'){
+        password2.setAttribute('type', 'text');
+        olhin2.classList.add('hide2');
+
+    }
+    else{
+        password2.setAttribute('type', 'password');
         olhin2.classList.remove('hide2');
     }
 
 }
+
+//Utilizando o ENTER para confirmar
+
+
+// inicio das verificações de cadastro
+
+// if(password1 == password2){
+
+// }
+// else{
+
+// }
