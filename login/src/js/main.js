2c7e2cb1d4c96a8d5ade44047d46ec659390cb84
Gusta <gutolights0@gmail.com>
2023-04-12 14:51:55 -0300

    Adição da função para deixar a senha visivel.

--- login/src/js/main.js
+++ login/src/js/main.js
@@ -1,3 +1,15 @@
 const password = document.querySelector('.password');
 const olhin = document.querySelector('.olhin');
 
+function showHide(){
+
+    if(password.type === 'password'){
+        password.setAttribute('type', 'text');
+        olhin.classList.add('hide');
+    }
+    else{
+        password.setAttribute('type', 'password');
+        olhin.classList.remove('hide');
+    }
+
+}
