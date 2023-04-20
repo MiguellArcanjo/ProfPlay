5145e23b8330712778c67c9d3a4d1b693fecc446
Z4D1 <gmrziko82@gmail.com>
2023-04-20 13:55:15 -0300

    Modificação no email

--- cadastro/processa.php
+++ cadastro/processa.php
@@ -1,4 +1,21 @@
 <?php
+$html = file_get_contents('paginaCadastro.html');
+
+$dom = new DOMDocument();
+$dom->LoadHTML($html);
+
+$element = $dom->getElementById('teste');
+
+$classes = explode(' ', $element->getAttribute('class'));
+
+if (in_array('teste2', $classes)) {
+    echo 'A classe "teste2" esta presente no elemento';
+}
+
+else {
+    echo 'A classe "minha classe não esta presente' ;
+}
+
 include("conexao.php");
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
@@ -9,15 +26,29 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $senha1 = $_POST['senha1'];
     $senha2 = $_POST['senha2'];
 
-    $sql = "INSERT INTO cadastro (email, datanascimento, nome, senha1, senha2) VALUES ('$email', '$datanascimento', '$nome', '$senha1', '$senha2')";
+    $verificar_email = "SELECT * FROM cadastro WHERE email='$email'";
+    $resultado = mysqli_query($conn, $verificar_email);
 
-
-    if (mysqli_query($conn, $sql)) {
-        echo "Mensagem enviada com sucesso!";
+    if (mysqli_num_rows($resultado) > 0) {
+        if (in_array('teste2', $classes)) {
+            echo 'A classe "teste2" esta presente no elemento';
+        }
+        
+        else {
+            echo 'A classe "minha classe não esta presente' ;
+        }
     }
-
     else {
-        echo "Erro ao enviar a mensagem" . mysqli_error($conn);
+
+        $sql = "INSERT INTO cadastro (email, datanascimento, nome, senha1, senha2) VALUES ('$email', '$datanascimento', '$nome', '$senha1', '$senha2')";
+    
+        if (mysqli_query($conn, $sql)) {
+            echo "Mensagem enviada com sucesso!";
+        }
+    
+        else {
+            echo "Erro ao enviar a mensagem" . mysqli_error($conn);
+        }
     }
 
     mysqli_close($conn);
